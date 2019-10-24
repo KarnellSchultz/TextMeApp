2ff61b4dbc4fef46f1145ec7fa73d5834c3ba6dc
karnell schultz <50870173+KarnellSchultz@users.noreply.github.com>
2019-10-24 15:59:54 +0200

    Add files via upload
    working app

--- public/client.js
+++ public/client.js
@@ -1,15 +1,15 @@
 let clientPhoneNumber = Number; 
 
-// document.getElementById("demo").addEventListener("click", myFunction);
-
 const pageElement = document.getElementById("body");
 const updateButton = document.getElementById("updateColor");
 const upcomingcolor = document.getElementById("upcomingcolor");
+const moreColors = document.getElementById("more-colors");
 
-let color = "DarkOrange"
+let color;
 pageElement.style.background = color;
 upcomingcolor.innerHTML = color
 updateButton.addEventListener("click", updateColors);
+moreColors.addEventListener("click", showColors);
 
 async function updateColors() {
   let colorData = await fetch('/colors');
@@ -17,7 +17,17 @@ async function updateColors() {
   pageElement.style.background = color;
   console.log(`The color is: ${color}`)
   upcomingcolor.innerHTML = color
-
 }
 
+// async function showColors() {
+//   let colorListData = await fetch('/colorsList');
+//   colorList = await colorListData.json();
+  
+//   colorList.forEach(element => {
+//     return moreColors.innerHTML = "<li>element</li>"
+//   });
+// }
+
 // setInterval(updateColors, 10000);
+
+
